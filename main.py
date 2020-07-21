eb1f78977c32ead3191b7db3c5cd151a7e23cf91
joshualau <joshualau2007@gmail.com>
2020-07-21 17:01:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+name1 = input ("What is your first name?")
+name2 = input ("What is your last name?")
+print("Hello " + name2 + " " + name1 )
+
+counter = 0
+for i in range(3):
+  counter += 1
+  print(counter)
